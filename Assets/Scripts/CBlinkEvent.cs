d24f5d2205ed8f3f1b833e5717543abc90de1853
SU <rumex13@naver.com>
2017-06-14 10:27:14 +0900

    통과 신 완성

--- Assets/Scripts/CBlinkEvent.cs
+++ Assets/Scripts/CBlinkEvent.cs
@@ -18,7 +18,7 @@ public class CBlinkEvent : MonoBehaviour
     public void BlinkEvent()
     {
         Vector2 endPos = new Vector2(checkPoint.position.x - ((movement.isRight) ? -checkRange : checkRange), checkPoint.position.y);
-        Collider2D hitCollision = Physics2D.OverlapCircle(endPos, 0.2f, blockingMask);
+        Collider2D hitCollision = Physics2D.OverlapCircle(endPos, 0.25f, blockingMask);
 
         Debug.DrawLine(checkPoint.position, endPos, Color.red);
 
---
+++ Assets/Scripts/CBombBox.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CBombBox : MonoBehaviour {
+
+	Animator animator;
+
+	void Awake()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.name == "Gnome")
+		{
+			animator.SetTrigger("Bomb");
+
+			StartCoroutine("BombCoroutine");
+		}
+	}
+
+	IEnumerator BombCoroutine()
+	{
+		yield return new WaitForSeconds(1.5f);
+
+		Collider2D[] hitColliders = Physics2D.OverlapCircleAll(transform.position, 0.5f, 1 <<LayerMask.NameToLayer("BlockingLayer"));
+
+		Debug.Log(hitColliders.Length);
+		foreach (Collider2D item in hitColliders)
+		{
+			if (item.tag == "Box")
+			{
+				item.SendMessage("Destroy");
+			}
+		}
+	}
+
+	void Destroy()
+	{
+		Destroy(gameObject);
+	}
+
+}
