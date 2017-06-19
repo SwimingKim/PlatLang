e608de9fc7368747cb35bf55a0808beeddcb17b6
SU <rumex13@naver.com>
2017-06-20 00:01:22 +0900

    몬스터 추가

---
+++ Assets/Scripts/CMonsterMovement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CMonsterMovement : MonoBehaviour {
+
+	public bool isRightDir = false;
+	Rigidbody2D _rigidbody2d;
+
+	public float _moveSpeed;
+
+	public GameObject _player;
+
+	void Awake()
+	{
+		_rigidbody2d = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		Move();
+	}
+
+	void Move()
+	{
+		_rigidbody2d.velocity = new Vector2(isRightDir ? _moveSpeed : -_moveSpeed, _rigidbody2d.velocity.y);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "TurnPoint")
+		{
+			Flip();
+		}
+	}
+
+	void Flip()
+	{
+		Vector2 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+		Debug.Log(transform.localScale);
+
+		isRightDir = !isRightDir;
+		Debug.Log("Flip");
+	}
+
+}
